e5b92d35b5589cab69ff95f08f5fab51857d717f
alaminBDZ <alaminbdz@yahoo.com>
2026-09-23 15:47:27 +0600

    Add second Dart Variables

---
+++ 2.variables.dart
@@ -0,0 +1,23 @@
+void main() {
+  // String
+  String name = "Alamin";
+
+  // Integer
+  int age = 20;
+
+  // Double
+  double cgpa = 3.50;
+
+  // Boolean
+  bool isStudent = true;
+
+  // Print variables
+  print("Name: $name");
+  print("Age: $age");
+  print("CGPA: $cgpa");
+  print("Student: $isStudent")
+
+  // String interpolation
+  print("$name is $age years old")
+
+}
